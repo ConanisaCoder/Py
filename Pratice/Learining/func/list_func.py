16c297cf9c5cb6904f41f5d3adee32bed107cbbc
ConanisaCoder <87452767+ConanisaCoder@users.noreply.github.com>
2025-01-09 21:29:31 -0500

    Add files via upload

---
+++ Pratice/Learining/func/list_func.py
@@ -0,0 +1,7 @@
+x = input("Enter string: ")
+y = input("Enter string: ")
+z = input("Enter string:")
+list_func = [x , y , z]
+run_program = input("Run program (y/n)?: ").lower()
+while run_program == "y":
+    print("(1) Add item(s) to list\n(2)Insert Item into list")
