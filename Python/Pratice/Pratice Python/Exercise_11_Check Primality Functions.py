e635505cbd360290c5607e52a67172f56ed0783e
ConanisaCoder <87452767+ConanisaCoder@users.noreply.github.com>
2025-01-23 15:20:19 -0500

    Create Exercise_11_Check Primality Functions.py

---
+++ Python/Pratice/Pratice Python/Exercise_11_Check Primality Functions.py
@@ -0,0 +1,10 @@
+num = int(input("Enter Number: "))
+if num <= 1:
+    print("not a prime Number")
+else:
+    for i in range(2,num):
+        if num // i == float:
+            print("Not a prime")
+            break
+    else:
+        print("Prime Number")
