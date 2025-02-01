d297c733a23874f75fb95a8bdbad1b8d9819e81b
ConanisaCoder <87452767+ConanisaCoder@users.noreply.github.com>
2025-02-01 14:17:06 -0500

    Add files via upload

--- Zebra Robotics/Zebra_Robtoics_Challenges/Challenge #48; Functions - Printing string as a list.py
+++ Zebra Robotics/Zebra_Robtoics_Challenges/Challenge #48; Functions - Printing string as a list.py
@@ -2,10 +2,10 @@
 Your program should take in a string and print out the string as a list. 
 Create a function called stringToList to help you do this.
 '''
-string = input("Enter string: ")
 def stringToList(string):
     stringToList_List = []
     for i in string:
         stringToList_List.append(i)
     print(stringToList_List)
+string = input("Enter string: ")
 stringToList(string)
--- Zebra Robotics/Zebra_Robtoics_Challenges/Challenge #49; Functions - Printing string in reverse.py
+++ Zebra Robotics/Zebra_Robtoics_Challenges/Challenge #49; Functions - Printing string in reverse.py
@@ -4,14 +4,12 @@ def stringToList(string,vaule=False):
         list_string.append(i)
     if vaule == True:
         list_string = list_string[::-1]
-        print(list_string)
         return list_string
     else: 
-        print(list_string)
         return list_string
 string = input("Enter string: ")
 vaule = input("Enter vaule(t/f): ").lower()
 if vaule == "t":
-    stringToList(string,True)
+    print(stringToList(string,True))
 elif vaule == "f":
-    stringToList(string,False)
+    print(stringToList(string,False))
