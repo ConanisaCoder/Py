8de9c841c12dd554aedbca2697862b732f1bfd88
ConanisaCoder <87452767+ConanisaCoder@users.noreply.github.com>
2025-02-05 19:48:03 -0500

    Add files via upload

--- Zebra Robotics/Zebra_Robtoics_Challenges/Challenge#56/ Challenge #56; Parsing Files -Figure out the encrypted message.py
+++ Zebra Robotics/Zebra_Robtoics_Challenges/Challenge#56/ Challenge #56; Parsing Files -Figure out the encrypted message.py
@@ -1,7 +1,14 @@
 readObj = open("/home/omega01/MyDrive/Zebra Robotics/Zebra_Robtoics_Challenges/Challenge#56/text.txt","r")
-file_list = readObj.readline()
-file_list = file_list.strip()
-val = int(file_list)
-print(val)
-
+readlines_readObj_encryption = readObj.readline()
+readlines_readObj_encryption.strip()
+val_change = int(readlines_readObj_encryption)
+newstring = ""
+while True:
+    retruns_lines = readObj.readlines()
+    if not retruns_lines:
+        break
+    retrun = "\n".join(retruns_lines)
+    for i in retrun:
+        newstring += chr(ord(i)+20)
+    print(newstring)
 readObj.close()
